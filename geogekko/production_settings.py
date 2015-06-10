05e445810315b49227e91ba85c422a7ca2ef7bdb
Rajiv Roopan <rajiv@Rajivs-MacBook-Pro-2.local>
2015-06-10 13:35:27 -0700

    updaets

--- geogekko/production_settings.py
+++ geogekko/production_settings.py
@@ -11,20 +11,4 @@ DATABASES = {
     }
 }
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/opt/bitnami/apps/django/django_projects/geogekko/templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 ELASTIC_SEARCH_HOST="http://localhost:9200/"
